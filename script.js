f096cd417223d9b303ecb5b98385f2c9080b9995
Bryce Eklund <bryce.eklund@gmail.com>
2019-05-14 12:26:18 -0700

    finished version 1.0

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-function scrollAnimation() {
-    $(document).ready(function() {
+/*function scrollAnimation() {
     console.log('`scrollAnimation is running...');
-    $('.projects-link').localScroll();
-    }); 
-}
 
-
+    $('#projects-link').click(function() {
+        $(window).scrollTo($('#project1', 1000));
+    });
+}*/
 
 function windowH(viewport) {
     if (viewport.matches) {
         var wH = $(window).height();
-    
         $('main').css({height: wH});
-        $('.project').css({height: wH});
     } else {
         $('main').css({height: '900px'});
     }
- }
+}
+
+$('.projects-link').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+        scrollTop: $('#project1').offset().top
+    }, 1000);
+});
 
  function titleFormat(viewport) {
      if (viewport.matches) {
          $('header h2').replaceWith('<h2>Full stack developer, musician, IT professional</h2>');
      }
  }
+
  
 let viewport = window.matchMedia("(min-width: 700px)")
+let viewportWidth = $(window).width();
 
- scrollAnimation();
  windowH(viewport);
  titleFormat(viewport);
